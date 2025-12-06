a25b14e52ae4ed105028e2e875305ea38a68c42e
bred_and_butter <fredericolarangeira@hotmail.com>
2025-12-06 17:27:02 -0300

    misfortune curse functional

---
+++ src/main/java/com/github/simplecurses/curses/FleshArmorCurse.java
@@ -0,0 +1,11 @@
+package com.github.simplecurses.curses;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+
+public class FleshArmorCurse extends Enchantment {
+    public FleshArmorCurse(Rarity rarity, EnchantmentCategory category, EquipmentSlot[] slots) {
+        super(rarity, category, slots);
+    }
+}
--- src/main/java/com/github/simplecurses/curses/MisfortuneCurse.java
+++ src/main/java/com/github/simplecurses/curses/MisfortuneCurse.java
@@ -1,6 +1,7 @@
 package com.github.simplecurses.curses;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.EquipmentSlot.Type;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -15,7 +16,16 @@ import java.util.UUID;
 public class MisfortuneCurse extends Enchantment {
 
     private final EquipmentSlot[] slots = EquipmentSlot.values();
-    private final AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(), "Curse of Misfortune", -10f, AttributeModifier.Operation.ADDITION);
+    //private final AttributeModifier modifier;
+
+    private static final UUID[] UUIDSlots = {
+            UUID.fromString("a04eebbc-f63d-4885-8e5b-f431e152307f"), //head
+            UUID.fromString("8126b90d-e3a6-41bf-bdba-64ccf4581caa"), //chest
+            UUID.fromString("24545cf0-55fe-4ebd-8236-6d093204725b"), //legs
+            UUID.fromString("b50fa1dc-2c99-4403-b7a6-c2a63c0bb666"), //feet
+            UUID.fromString("5892abe8-6883-4686-9d78-0658ee40c800"), //mainhand tools
+            UUID.fromString("e4e96284-9a02-430d-8563-71af43090c08"), //offhand tools
+    };
 
     public MisfortuneCurse() {
 		super(Rarity.UNCOMMON, EnchantmentCategory.BREAKABLE, EquipmentSlot.values());
@@ -38,16 +48,33 @@ public class MisfortuneCurse extends Enchantment {
     }
 
     private void checkModifiers(ItemAttributeModifierEvent event) {
-        if (event.getSlotType() == LivingEntity.getEquipmentSlotForItem(event.getItemStack()) && this.isValidSlot(event.getSlotType()) && event.getItemStack().hasTag()) {
+        EquipmentSlot slot = event.getSlotType();
+
+        if (slot == LivingEntity.getEquipmentSlotForItem(event.getItemStack()) && this.isValidSlot(slot) && event.getItemStack().hasTag()) {
             final int level = EnchantmentHelper.getTagEnchantmentLevel(this, event.getItemStack());
-            this.applyModifiers(level, event);
+
+            int slotIndex = switch (slot) {
+                case HEAD -> 0;
+                case CHEST -> 1;
+                case LEGS -> 2;
+                case FEET -> 3;
+                case MAINHAND -> 4;
+                case OFFHAND -> 5;
+            };
+
+            this.applyModifiers(level, slotIndex, event);
         }
     }
 
-    public void applyModifiers(int level, ItemAttributeModifierEvent event) {
+    public void applyModifiers(int level, int slotIndex, ItemAttributeModifierEvent event) {
         if (level > 0) {
-            event.addModifier(Attributes.LUCK, this.modifier);
+            AttributeModifier modifier = new AttributeModifier(
+                    UUIDSlots[slotIndex],
+                    "Curse of Misfortune",
+                    -10f,
+                    AttributeModifier.Operation.ADDITION);
+
+            event.addModifier(Attributes.LUCK, modifier);
         }
     }
-    
 }
